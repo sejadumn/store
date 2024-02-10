d0c30715e1b7d40d849c4c550a0b35bb2839465e
Сергей Дудченко <sejadamn@outlook.com>
2024-02-10 19:38:59 +0700

    Добавить интерфейс репрозитория книг

---
+++ domain/Store/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    internal interface IBookRepository
+    {
+        Book[] GetByTitle(string titlePart);
+    }
+}
