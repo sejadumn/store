57d7517edc01c3c872fea3f6ee53295e688f9a6a
Сергей Дудченко <sejadamn@outlook.com>
2024-02-11 14:19:44 +0700

    Создание веб-приложения

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -2,6 +2,17 @@
 {
     public class BookRepository : IBookRepository 
     {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language")
+        };
 
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(books => books.Title.Contains(titlePart))
+                        .ToArray();
+        }
     }
 }
